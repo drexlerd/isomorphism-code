7c8ab68456d2d4fa5906e8ffa64831b76aa65618
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-23 21:32:14 +0100

    added experiment to compare directed vs undirected

--- experiments/2023-02-21-naive.py
+++ experiments/2023-02-21-naive.py
@@ -98,6 +98,7 @@ exp.add_parser(IsomorphismParser())
 #exp.add_resource("main_script", REPO / "main.py", symlink=True)
 
 for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
+    ### Undirected
     run = exp.add_run()
     # Create symbolic links and aliases. This is optional. We
     # could also use absolute paths in add_command().
@@ -116,7 +117,7 @@ for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
     # 'domain', 'problem', 'algorithm', 'coverage'.
     run.set_property("domain", task.domain)
     run.set_property("problem", task.problem)
-    run.set_property("algorithm", "exact")
+    run.set_property("algorithm", "exact-undirected")
     # BaseReport needs the following properties:
     # 'time_limit', 'memory_limit'.
     run.set_property("time_limit", TIME_LIMIT)
@@ -124,7 +125,36 @@ for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
     # Every run has to have a unique id in the form of a list.
     # The algorithm name is only really needed when there are
     # multiple algorithms.
-    run.set_property("id", ["exact", task.domain, task.problem])
+    run.set_property("id", ["exact-undirected", task.domain, task.problem])
+
+    ### Directed
+    run = exp.add_run()
+    # Create symbolic links and aliases. This is optional. We
+    # could also use absolute paths in add_command().
+    run.add_resource("domain", task.domain_file, symlink=True)
+    run.add_resource("problem", task.problem_file, symlink=True)
+    run.add_resource("main_script", REPO / "main.py", symlink=True)
+    # 'ff' binary has to be on the PATH.
+    # We could also use exp.add_resource().
+    run.add_command(
+        "main_script_exact",
+        ["python", "{main_script}", "exact", "--domain_file_path", "{domain}", "--problem_file_path", "{problem}", "--enable-pruning"],
+        time_limit=TIME_LIMIT,
+        memory_limit=MEMORY_LIMIT,
+    )
+    # AbsoluteReport needs the following properties:
+    # 'domain', 'problem', 'algorithm', 'coverage'.
+    run.set_property("domain", task.domain)
+    run.set_property("problem", task.problem)
+    run.set_property("algorithm", "exact-directed")
+    # BaseReport needs the following properties:
+    # 'time_limit', 'memory_limit'.
+    run.set_property("time_limit", TIME_LIMIT)
+    run.set_property("memory_limit", MEMORY_LIMIT)
+    # Every run has to have a unique id in the form of a list.
+    # The algorithm name is only really needed when there are
+    # multiple algorithms.
+    run.set_property("id", ["exact-directed", task.domain, task.problem])
 
 # Add step that writes experiment files to disk.
 exp.add_step("build", exp.build)
