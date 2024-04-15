fc332f63a0711de0a83a093b5cb7fae6562fc922
Simon <simon.stahlberg@gmail.com>
2024-04-15 13:50:58 +0200

    Update driver

--- src/wl_analysis.py
+++ src/wl_analysis.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from pathlib import Path
 from pymimir import DomainParser, ProblemParser, Domain, Problem, GroundedSuccessorGenerator, StateSpace, State
 from tqdm import tqdm
-from typing import Any, Dict, List, Set, Tuple
+from typing import List, Tuple
 
 from .logger import initialize_logger, add_console_handler
 from .state_graph import StateGraph
@@ -46,16 +46,22 @@ def to_dec_graph(state: State) -> WLGraph:
             dst_vertex = to_wl_vertex[atom.terms[1]]
             wl_graph.add_edge(src_vertex, dst_vertex, atom.predicate.id + offset)
         else:
-            raise Exception("predicate arity is not 1 or 2")
-    # TODO: Add type edges.
-    # Add state edges.
-    for atom in state.get_atoms():
-        add_labeled_edge(atom, 0)
-    # Add goal edges.
-    for literal in state.get_problem().goal:
-        assert not literal.negated
-        add_labeled_edge(literal.atom, len(state.get_problem().domain.predicates))
-    return wl_graph
+            raise RuntimeError("ternary predicate")
+    # Add edges.
+    try:
+        # Add state edges.
+        for atom in state.get_atoms():
+            add_labeled_edge(atom, 0)
+        # Add goal edges.
+        for literal in state.get_problem().goal:
+            assert not literal.negated
+            add_labeled_edge(literal.atom, len(state.get_problem().domain.predicates))
+        # Add types
+        if len(state.get_problem().domain.types) > 1:
+            raise NotImplementedError()
+        return wl_graph
+    except:
+        return None
 
 
 class Driver:
@@ -88,18 +94,32 @@ class Driver:
         for state in tqdm(states, mininterval=0.5):
             state_graph = StateGraph(state, skip_nauty=False)
             exact_key = state_graph.nauty_certificate, state_graph.uvc_graph.get_colors()
-            partitions[exact_key].append(state_graph.state)
+            partitions[exact_key].append(state)
+        return list(partitions.values())
+
+
+    def _partition_with_wl(self, states: List[State], k, to_graph) -> List[List[State]]:
+        partitions = defaultdict(list)
+        wl = WeisfeilerLeman(k)
+        for state in tqdm(states, mininterval=0.5):
+            wl_graph = to_graph(state)
+            wl_key = wl.compute_coloring(wl_graph)
+            partitions[wl_key].append(state)
         return list(partitions.values())
 
 
     def _validate_wl_correctness(self, partitions: List[List[State]], k, to_graph) -> Tuple[bool, int]:
+        # Check whether the to_graph function can handle states of this sort.
+        if to_graph(partitions[0][0]) is None: return False, -1
+        # Test representatives from each partition to see if two are mapped to the same class.
         correct = True
         conflicts = 0
         wl = WeisfeilerLeman(k)
         state_colorings = {}
         for partition in tqdm(partitions, mininterval=0.5):
             representative = partition[0]
-            coloring = wl.compute_coloring(to_graph(representative))
+            wl_graph = to_graph(representative)
+            coloring = wl.compute_coloring(wl_graph)
             if coloring in state_colorings:
                 correct = False
                 conflicts += 1
@@ -126,27 +146,38 @@ class Driver:
             return
         self._logger.info(f"[Preprocessing] States: {len(states)}")
 
-        # Generate exact equivalence classes
+        # Generate exact equivalence classes.
         partitions = self._partition_with_nauty(states)
+        self._logger.info(f"[Nauty] Partitions: {len(partitions)} ")
 
-        # Validate 1-FWL on the DEC graph
+        # If WL is implemented correctly, then validating correctness the way we do is safe.
+        # However, double check by partitioning with WL and if more partitions than with Nauty is found, then something is wrong.
+        if self._verbosity == "DEBUG":
+            uvc_wl_1_partitions = self._partition_with_wl(states, 1, to_uvc_graph)
+            self._logger.info(f"[DEBUG] 1-WL partitions: {len(uvc_wl_1_partitions)} ")
+
+        # Validate 1-FWL on the DEC graph.
         self._logger.info("[1-FWL, DEC] Validating...")
         dec_correct_1, dec_conflicts_1 = self._validate_wl_correctness(partitions, 1, to_dec_graph)
-        self._logger.info(f"[1-FWL, DEC] Valid: {dec_correct_1}; Conflicts: {dec_conflicts_1}")
+        if (not dec_correct_1) and (dec_conflicts_1 < 0): self._logger.info(f"[1-FWL, DEC] Graph cannot be constructed. Skipping.")
+        else: self._logger.info(f"[1-FWL, DEC] Valid: {dec_correct_1}; Conflicts: {dec_conflicts_1}")
 
-        # Validate 1-FWL on the UVC graph
+        # Validate 1-FWL on the UVC graph.
         self._logger.info("[1-FWL, UVC] Validating...")
         uvc_correct_1, uvc_conflicts_1 = self._validate_wl_correctness(partitions, 1, to_uvc_graph)
-        self._logger.info(f"[1-FWL, UVC] Valid: {uvc_correct_1}; Conflicts: {uvc_conflicts_1}")
+        if (not uvc_correct_1) and (uvc_conflicts_1 < 0): self._logger.info(f"[1-FWL, UVC] Graph cannot be constructed. Skipping.")
+        else: self._logger.info(f"[1-FWL, UVC] Valid: {uvc_correct_1}; Conflicts: {uvc_conflicts_1}")
 
-        # If 1-FWL produces conflicts on the DEC graph, test 2-FWL
+        # If 1-FWL produces conflicts on the DEC graph, test 2-FWL.
         if not dec_correct_1:
             self._logger.info("[2-FWL, DEC] Validating...")
             dec_correct_2, dec_conflicts_2 = self._validate_wl_correctness(partitions, 2, to_dec_graph)
-            self._logger.info(f"[2-FWL, DEC] Valid: {dec_correct_2}; Conflicts: {dec_conflicts_2}")
+            if (not dec_correct_2) and (dec_conflicts_2 < 0): self._logger.info(f"[2-FWL, DEC] Graph cannot be constructed. Skipping.")
+            else: self._logger.info(f"[2-FWL, DEC] Valid: {dec_correct_2}; Conflicts: {dec_conflicts_2}")
 
-        # If 1-FWL produces conflicts on the UVC graph, test 2-FWL
+        # If 1-FWL produces conflicts on the UVC graph, test 2-FWL.
         if not uvc_correct_1:
             self._logger.info("[2-FWL, UVC] Validating...")
             uvc_correct_2, uvc_conflicts_2 = self._validate_wl_correctness(partitions, 2, to_uvc_graph)
-            self._logger.info(f"[2-FWL, UVC] Valid: {uvc_correct_2}; Conflicts: {uvc_conflicts_2}")
+            if (not uvc_correct_2) and (uvc_conflicts_2 < 0): self._logger.info(f"[2-FWL, UVC] Graph cannot be constructed. Skipping.")
+            else: self._logger.info(f"[2-FWL, UVC] Valid: {uvc_correct_2}; Conflicts: {uvc_conflicts_2}")
